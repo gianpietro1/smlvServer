abae462f2715708998cce7169d348a25a243cdf7
lavado <glavado@whitestack.com>
2020-07-18 09:41:25 -0500

    update deployment info

---
+++ src/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.mongoURI,
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  cryptoKey: process.env.cryptoKey,
+};
