4b9357f5313fb3f6d657e9cc8c10951476bea2c8
lavado <glavado@whitestack.com>
2020-07-20 12:22:58 -0500

    temporarily restore project memberData in route response

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -8,8 +8,19 @@ const Member = mongoose.model("Member");
 const router = express.Router();
 
 router.get("/projects", async (req, res) => {
-  const projects = await Project.find().sort({ name: 1 });
-  res.send(projects);
+  const projects = await Project.find()
+    .sort({ name: 1 })
+    .lean();
+  const promises = projects.map(async (project) => {
+    return {
+      ...project,
+      memberData: await Member.findOne({
+        _id: project._member,
+      }),
+    };
+  });
+  const newProjects = await Promise.all(promises);
+  res.send(newProjects);
 });
 
 router.post("/projects", requireAuth, async (req, res) => {
@@ -17,6 +28,7 @@ router.post("/projects", requireAuth, async (req, res) => {
   if (!name) {
     return res.status(422).send({ error: "You must provide a name" });
   }
+
   try {
     const project = new Project(req.body);
     await project.save();
@@ -36,6 +48,7 @@ router.put("/projects/:id", requireAuth, async (req, res) => {
 router.delete("/projects/:id", requireAuth, async (req, res) => {
   await Project.deleteOne({ _id: req.params.id }, function(err) {
     if (err) return res.status(422).send({ error: "Error deleting the entry" });
+    // deleted at most one tank document
   });
   res.send(`${req.params.id} deleted.`);
 });
