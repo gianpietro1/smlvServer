fc7943c236487bba9cf933d36e52b50ec4c41102
gianpietro1 <gianpietro1@gmail.com>
2021-01-17 15:31:30 -0500

    retry mongo

--- src/index.js
+++ src/index.js
@@ -33,6 +33,27 @@ mongoose.connection.on("error", (err) =>
   console.log("Error connecting to mongo", err)
 );
 
+// Mongoose connect
+const mongooseConnect = () => {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+};
+
+/// Retry connection
+const connectWithRetry = () => {
+  console.log("MongoDB connection with retry");
+  return mongooseConnect();
+};
+
+/// Exit application on error
+mongoose.connection.on("error", (err) => {
+  console.log(`MongoDB connection error: ${err}`);
+  setTimeout(connectWithRetry, 5000);
+});
+
 // Static routes
 app.get("/", (req, res) => {
   res.send("magiqapps!");
